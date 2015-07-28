a82195e2056b62e1e13dd72b181591cd02c9711a
liquid11 <orangevolution@yahoo.com>
2015-07-29 01:42:11 +0500

    fix constrain errors

--- sound fiddler/ViewController.swift
+++ sound fiddler/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+   
+    
+       override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnRecord(sender: UIButton) {
+//TODO:recording in progress
+        //TODO:record the users voice
+        
+    }
 
 }
 
