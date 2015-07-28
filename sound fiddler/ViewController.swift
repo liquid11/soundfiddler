a8a2d287554be7a71b703869e173685bc3a07884
liquid11 <orangevolution@yahoo.com>
2015-07-28 23:05:29 +0500

    Initial Commit

---
+++ sound fiddler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sound fiddler
+//
+//  Created by Tezro Solutions on 28/07/2015.
+//  Copyright (c) 2015 Tezro Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
